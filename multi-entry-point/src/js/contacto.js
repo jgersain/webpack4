7dc726e36dab4ca46460a4760c49152e7766101d
jgersain <jhersein@gmail.com>
2019-09-11 23:36:10 -0500

    manejando multiples puntos de entrada

---
+++ multi-entry-point/src/js/contacto.js
@@ -0,0 +1,1 @@
+console.log("hola, esto es contacto.js");
---
+++ multi-entry-point/webpack.config.js
@@ -0,0 +1,15 @@
+// modulo necesario para trabajar con rutas de archivos
+const path = require('path')
+
+module.exports = {
+  entry: { 
+    home: path.resolve(__dirname, 'src/js/index.js'),
+    precios: path.resolve(__dirname, 'src/js/precios.js'),
+    contacto: path.resolve(__dirname, 'src/js/contacto.js'),
+  },
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js'
+  }
+}
