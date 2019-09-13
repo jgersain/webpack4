98c9f2cb27a5b1c706d3b3cf039fa7f463145501
jgersain <jhersein@gmail.com>
2019-09-13 00:30:41 -0500

    ejemplo utilizando js moderno

--- babel/src/js/index.js
+++ babel/src/js/index.js
@@ -1,11 +1,13 @@
 import '../css/index.css'
-import text from './text'
+import search from './search'
+import render from './render'
 
-text()
-
-if (module.hot) {
-  module.hot.accept('./text.js', function() {
-    console.log("se ha recargado en caliente...");
-    text()
+const id = prompt('quien es ese pokemon')
+search(id)
+  .then(data => {
+    render(data)
   })
-}
+  .catch(() => {
+    console.log('No hubo pokemon');
+  })
+
--- babel/src/js/text.js
+++ babel/src/js/text.js
@@ -1,4 +1,4 @@
-function text() {
+const text = () => {
   document.body.innerHTML = "<p>Webpack rules!... shit, it works</p>"
   console.log("hola, esto es home.js")
 }
