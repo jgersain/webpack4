893fdef588eff6b657edd60aacb27f5c3090c928
jgersain <jhersein@gmail.com>
2019-09-14 22:42:38 -0500

    aplicación simple en react utilizando lo aprendido con webpack

--- react/src/js/components/app.js
+++ react/src/js/components/app.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import data from './data.json'
+import Loader from './loader'
 
 function App() {
+  const [loaderList, setLoaderList] = useState([])
+
+  function handleClick() {
+    setLoaderList(data.loaders)
+  }
+
   return (
     <div>
-      esta es una aplicacion hecha con react
+      Aplicación hecha en React
+      <ul>
+        {
+          loaderList.map(item => <Loader {... item} key={item.id} />)
+        }
+      </ul>
+      <button onClick={ handleClick }>Mostrar lo apendido</button>
     </div>
   )
 }
