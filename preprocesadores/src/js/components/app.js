be63fe2bd376aae654c05946de1d3de3f7bd63d7
jgersain <jhersein@gmail.com>
2019-09-15 00:16:47 -0500

    agregar soporte para less, stylus, sass y postcss

---
+++ preprocesadores/src/js/components/app.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import data from './data.json'
+import Loader from './loader'
+import logo from '../../images/wallpapper02.jpg'
+import video from '../../video/que-es-core.mp4'
+import '../../less/less.less'
+import '../../sass/sass.scss'
+import '../../stylus/stylus.styl'
+
+function App() {
+  const [loaderList, setLoaderList] = useState([])
+
+  function handleClick() {
+    setLoaderList(data.loaders)
+  }
+
+  return (
+    <div>
+      <p className="sass">
+        Esto contiene estilos con sass
+      </p>
+      <p className="less">
+        Esto contiene estilos con less
+      </p>
+      <p className="stylus">
+        Esto contiene estilos con stylus
+      </p>
+      <p className="post-css">
+        <p className="uno">
+          Prueba uno
+        </p>
+        <p className="dos">
+          Prueba dos
+        </p>
+        Esto contiene estilos con post css
+      </p>
+      <p>
+        Aplicación hecha en React
+      </p>
+      <video 
+        src={ video } 
+        width={360} 
+        controls 
+        poster={logo}
+      >
+      </video>
+      <p>
+        <img
+          height="200"
+          width="300" 
+          src={ logo } 
+          alt="imagen de prueba"
+        />
+      </p>
+      <ul>
+        {
+          loaderList.map(item => <Loader {... item} key={item.id} />)
+        }
+      </ul>
+      <button onClick={ handleClick }>Mostrar lo apendido</button>
+    </div>
+  )
+}
+
+export default App
---
+++ preprocesadores/src/js/text.js
@@ -0,0 +1,6 @@
+const text = () => {
+  document.body.innerHTML = "<p>Webpack rules!... shit, it works</p>"
+  console.log("hola, esto es home.js")
+}
+
+export default text
